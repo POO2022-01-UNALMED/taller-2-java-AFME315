fd2620cc07acd940196184ca46c806f5e91655d2
Andres Mena <101481540+AFME315@users.noreply.github.com>
2022-03-22 11:33:41 -0500

    Update Auto.java

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -4,23 +4,23 @@ package test;
 public class Auto {
 	String modelo;
 	int precio;
-	Asiento asientos[];
+	Asiento[] asientos;
 	String marca;
 	Motor motor;
 	int registro;
-	public static int cantidadCreados;
+	static int cantidadCreados;
 	
 	int cantidadAsientos(){
 		int totalAsientos=0;
-		for (Asiento i:this.asientos) {
-			if(i!=null)
-				totalAsientos++;
+		for (Asiento u:asientos) {
+			if(u!=null){
+			++totalAsientos;}
 			}
 		return totalAsientos;
-	}
+		}
 	String verificarIntegridad() {
-		if (this.registro != this.motor.registro)
-			return "Las piesas no son originales";
+		if (this.motor.registro != this.registro)
+		{return "Las piesas no son originales";}
 		
 		for(int i=0;i<asientos.length;i++) {
 				if (asientos[i] !=null && (asientos[i].registro != registro)){
