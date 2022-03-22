a5e2f6297ef4b94c30500e93b8db11b5a6090ea1
Andres Mena <101481540+AFME315@users.noreply.github.com>
2022-03-22 10:37:16 -0500

    Update Auto.java

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -1,44 +1,41 @@
 package test;
 
 public class Auto {
-	String modelo;
-	int precio;
-	Asiento[] asientos;
-	Motor motor;
-	int registro;			
-	static int cantidadCreados;
+    String modelo;
+    int precio;
+    Asiento asientos[];
+    String marca;
+    Motor motor;
+    int registro;
+    static int cantidadCreados;
+
+    int cantidadAsientos(){
+        int contador=0;
+        for(int i=0;i<asientos.length;i++){
+            if (asientos[i]!=null){
+                contador++;
+            }
+        }
+        return contador;
+    }
+
+    String verificarIntegridad(){
+        for(int i=0;i<asientos.length;i++){
+            if (asientos[i]!=null){
+                if(((asientos[i]).registro==registro) & (motor.registro==registro) & ((asientos[i]).registro==motor.registro)){
+                    return "Auto original";
+                }
+                return "Las piezas no son originales";
+            }
+        }
+        return "Las piezas no son originales";
+        
+        
+        
+        
+    }
 
 
-	
-	int cantidadAsientos()
-	{
-		int totalAsientos=0;
-		for (int i =0; i < asientos.length; i++)
-		{ 
-			if (asientos[i] != null)
-			{
-				totalAsientos++;
-			}
-		}
-		return totalAsientos;
-	}
-
-	String verificarIntegridad()
-	{
-        	for(int i=0; i<asientos.length ; i++)
-		{
-           	 if (asientos[i]!=null)
-			{
-               		 if(((asientos[i]).registro==registro) & (motor.registro==registro) & ((asientos[i]).registro==motor.registro))
-			{
-                   	 return "Auto original";
-               		 }
-                	return "Las piezas no son originales";
-           	 }
-       	 	}
-        	return "Las piezas no son originales";
-
-	}
+    
 }
-
 	
